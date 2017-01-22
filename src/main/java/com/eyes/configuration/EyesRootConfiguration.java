12e01673de9c2025124ac89866eb00eae5322b04
Trever Pietsch <tpietsch@Trevers-MacBook-Pro-2.local>
2017-01-21 21:54:25 -0800

    starting to move toward annotation driven config

---
+++ src/main/java/com/eyes/configuration/EyesRootConfiguration.java
@@ -0,0 +1,12 @@
+package com.eyes.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
+
+/**
+ * Created by tpietsch on 1/21/17.
+ */
+@Configuration
+public class EyesRootConfiguration {
+}
